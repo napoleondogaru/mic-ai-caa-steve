b61592b66243443e6e2b0d9363a7f2db9025d138
Napoleon Leonard Dogaru <135789429+napoleondogaru@users.noreply.github.com>
2023-11-19 09:00:26 +0000

    rebuild 3

--- frontend/src/pages/layout/Layout.tsx
+++ frontend/src/pages/layout/Layout.tsx
@@ -79,7 +79,7 @@ const Layout = () => {
                             aria-hidden="true"
                         />
                         <Link to="/" className={styles.headerTitleContainer}>
-                            <h1 className={styles.headerTitle}>My Name is Steve - Your Assessor Copilot!)</h1>
+                            <h1 className={styles.headerTitle}>I am Steve - Your Assessor Copilot!</h1>
                         </Link>
                     </Stack>
                     <Stack horizontal tokens={{ childrenGap: 4 }}>
